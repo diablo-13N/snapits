87624418a179c9432e5354326be74afdc0b1d5db
diablo-13N <81568999+diablo-13N@users.noreply.github.com>
2021-08-20 07:10:54 +0530

    Create Snamp.py

---
+++ Snamp.py
@@ -0,0 +1,9 @@
+import zlib
+text = b"Python Pool!" * 100
+compressed = zlib.compress(text)
+ 
+print("size of original: " + str(len(text)))
+print("size of compressed: " + str(len(compressed)))
+ 
+decompressed = zlib.decompress(compressed)
+print("size of decompressed: " + str(len(decompressed)))
